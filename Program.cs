b9f28ff8fea0bece90f713d59bfca0bfd71c6d17
Amar AA <amar.ashfaq95@gmail.com>
2025-12-19 00:36:06 +0000

    Registered the Jwt Authentication in the main application. Created a Jwt Secret Key and stored it in Secret Storage.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using TaskFlowAPI.Data;
 using TaskFlowAPI.Repositories;
 using TaskFlowAPI.Services;
@@ -15,6 +18,31 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Add Jwt Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    var jwtSection = builder.Configuration.GetSection("Jwt");
+    var secretKey = jwtSection.GetValue<string>("SecretKey");
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSection.GetValue<string>("Issuer"),
+        ValidAudience = jwtSection.GetValue<string>("Audience"),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 // Register Dependancy Injection
 builder.Services.AddScoped<ITaskFlowRepository, TaskFlowRepository>();
 builder.Services.AddScoped<ITaskFlowService, TaskFlowService>();
@@ -30,6 +58,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
